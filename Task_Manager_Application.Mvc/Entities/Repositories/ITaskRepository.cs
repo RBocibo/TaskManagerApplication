68be617f5e283ee02029d781db018001b0ee2aa2
rbocibo <rbocibo@jhb.dvt.co.za>
2024-02-26 14:04:03 +0200

    :high_brightness: Added code to create task, edit task and view tasks :gem:

---
+++ Task_Manager_Application.Mvc/Entities/Repositories/ITaskRepository.cs
@@ -0,0 +1,9 @@
+﻿using Task_Manager_Application.Mvc.Infrastructure.Repository;
+
+namespace Task_Manager_Application.Mvc.Entities.Repositories
+{
+    public interface ITaskRepository : IBaseRepository<Models.Task>
+    {
+
+    }
+}
